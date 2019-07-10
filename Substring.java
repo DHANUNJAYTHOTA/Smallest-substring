eefaa0f8185cb295bdf2504f4515517bf9c4c400
THOTA DHANUNJAY <47811497+DHANUNJAYTHOTA@users.noreply.github.com>
2019-07-10 23:21:47 +0530

    Update Substring.java

--- Substring.java
+++ Substring.java
@@ -1,45 +1,45 @@
-import java.io.*;
-import java.util.*;
-class Substring
-{
-public static void main(String args[])
-{
-public static String getSmallest(String s, int k) {
-        String smallest = ""; 
-        if(s.length() > k)
-        {
-        for(int i = 0; i < (s.length() - k);i++)
-        {
-            if(i == 0)
-            {
-              
-                smallest = s.substring(0,k);
+public class Sub { 
+  
+    static final int NO_OF_CHARS = 256; 
+  
+    static int longestUniqueSubsttr(String str) 
+    { 
+        int n = str.length(); 
+        int cur_len = 1;  
+        int max_len = 1; 
+        int prev_index; 
+        int i; 
+        int visited[] = new int[NO_OF_CHARS]; 
+        for (i = 0; i < NO_OF_CHARS; i++) { 
+            visited[i] = -1; 
+        } 
+  
+       
+        visited[str.charAt(0)] = 0; 
+  for (i = 1; i < n; i++) { 
+            prev_index = visited[str.charAt(i)]; 
+   if (prev_index == -1 || i - cur_len > prev_index) 
+                cur_len++; 
+             else { 
                
-            }
-            
-            String sm1 = s.substring(i+1,i+k+1);
-          
-            
-            if(sm1.compareTo(smallest) < 0)
-            {
-                smallest = sm1;
-                
-            }
-        else
-        {
-            smallest = s;
-            
-        }
-        
-        return smallest;
-    }
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.next();
-        scan.close();
-        System.out.println(getSmallest(s));
-        System.out.println(s.length();
-        
-    }
-}
+                if (cur_len > max_len) 
+                    max_len = cur_len; 
+  
+                cur_len = i - prev_index; 
+            } 
+  
+            visited[str.charAt(i)] = i; 
+        } 
+        if (cur_len > max_len) 
+            max_len = cur_len; 
+  
+        return max_len; 
+    } 
+  public static void main(String[] args) 
+    { 
+        String str = "abca"; 
+        System.out.println("The input string is " + str); 
+        int len = longestUniqueSubsttr(str); 
+        System.out.println(len); 
+    } 
+} 
